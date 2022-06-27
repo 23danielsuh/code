55665db007e6b61ed1177c7c5c0ca41e446bb6c1
Daniel Suh <63919918+23danielsuh@users.noreply.github.com>
2022-06-27 12:01:15 -0400

    finished quiz game gophercise

--- go/gophercises/quiz_game/main.go
+++ go/gophercises/quiz_game/main.go
@@ -1,18 +1,59 @@
 package main
 
 import (
-	"encoding/csv"
-	"fmt"
-	"io"
-	"log"
-	"os"
+  "encoding/csv"
+	"flag"
+  "fmt"
+  "io"
+  "log"
+  "os"
+  "time"
 )
 
-func hello() {
-	fmt.Println("Hello world!")
+type problem struct {
+  q string
+  a string
 }
 
-func readCSVFile(filePath string) {
+func main() {
+	csvFilename := flag.String("csv", "problems.csv", "a csv file with the format of question,answer")
+	timeLimit := flag.Int("time", 15, "the time limit for the quiz in seconds")
+	flag.Parse()
+
+  problems := readCSVFile(*csvFilename)
+  timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+
+  numCorrect, numQuestions := 0, len(problems)
+
+problemloop:
+  for i, problem := range problems {
+		fmt.Printf("Problem #%d: %s = ", i + 1, problem.q)
+
+		answerCh := make(chan string)
+
+		go func() {
+			var str string
+			fmt.Scanf("%s", &str)
+			answerCh <- str
+		}()
+
+		select {
+		case answer := <-answerCh:
+			if answer == problem.a {
+				numCorrect++
+			}
+		case <-timer.C:
+			fmt.Println()
+			break problemloop
+		}
+	}
+			
+	fmt.Printf("You scored %d out of %d.\n", numCorrect, numQuestions)
+}
+
+func readCSVFile(filePath string) []problem {
+  problems := make([]problem, 0)
+
   file, err := os.Open(filePath)
   if err != nil {
     log.Fatal(err)
@@ -31,10 +72,9 @@ func readCSVFile(filePath string) {
       log.Fatal(err)
     }
     
-    fmt.Println(line[0])
+    problems = append(problems, problem{line[0], line[1]})
   }
+
+  return problems
 }
 
-func main() {
-  readCSVFile("problems.csv")
-}
