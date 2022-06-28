c37a1c2b8a38a04f657a360e0c5a46ab48050050
Daniel Suh <63919918+23danielsuh@users.noreply.github.com>
2022-06-28 15:00:33 -0400

    additional features for task_list.go

--- go/task_list/main.go
+++ go/task_list/main.go
@@ -11,31 +11,39 @@ import (
 type model struct {
 	tasks          []string
 	cursor         int
-	selected       map[int]bool
+	completed      map[int]bool
 	textInput      textinput.Model
 	textInputFocus bool
+	editing        bool
 }
 
 func initialModel() model {
 	ti := textinput.New()
 	ti.Placeholder = "New task"
 	ti.CharLimit = 250
+	ti.Focus()
+	ti.Prompt = ""
 	ti.Width = 20
 
 	return model{
-		tasks:          []string{"testing", "testing", "testing"},
-		selected:       make(map[int]bool),
+		tasks:          []string{},
+		completed:      make(map[int]bool),
 		textInput:      ti,
-		textInputFocus: false,
+		textInputFocus: true,
+		editing:        false,
 	}
 }
 
 func (m model) Init() tea.Cmd {
-	return textinput.Blink
+	return nil
 }
 
 func mod(x, m int) int {
 	return (x%m + m) % m
+}
+func remove(slice []string, i int) []string {
+	copy(slice[i:], slice[i+1:])
+	return slice[:len(slice)-1]
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -56,23 +64,42 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				m.cursor++
 				m.cursor = mod(m.cursor, len(m.tasks))
 
-			case "enter", " ":
-				task_done := m.selected[m.cursor]
+			case " ":
+				task_done := m.completed[m.cursor]
 				if task_done {
-					m.selected[m.cursor] = false
+					m.completed[m.cursor] = false
 				} else {
-					m.selected[m.cursor] = true
+					m.completed[m.cursor] = true
 				}
+
+			case "d":
+				m.tasks = remove(m.tasks, m.cursor)
+
+			case "enter":
+				m.editing = true
+				m.textInputFocus = true
+				m.textInput.Focus()
 
 			case "o":
 				m.textInputFocus = true
+				m.textInput.Focus()
 			}
 		} else {
 			switch msg.String() {
+			case "ctrl+c", "q":
+				return m, tea.Quit
+
 			case "enter":
-                m.tasks = append(m.tasks, m.textInput.Value())
-                m.textInputFocus = false
-                m.textInput.Reset()
+				if m.editing {
+					m.tasks[m.cursor] = m.textInput.Value()
+					m.editing = false
+				} else {
+					m.tasks = append(m.tasks, m.textInput.Value())
+					m.cursor = len(m.tasks) - 1
+				}
+				m.textInputFocus = false
+				m.textInput.Reset()
+
 			default:
 				m.textInput, cmd = m.textInput.Update(msg)
 				m.textInput.Focus()
@@ -84,23 +111,36 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
+	allCompleted := true
+
 	s := "Task list (type o to add task):\n\n"
 
 	for i, choice := range m.tasks {
+		if m.editing && i == m.cursor {
+			s += fmt.Sprintf("  [ ] %s\n", m.textInput.View())
+			continue
+		}
 		cursor := " "
 		if m.cursor == i && !m.textInputFocus {
 			cursor = ">"
 		}
 
 		checked := " "
-		if b := m.selected[i]; b {
+		if b := m.completed[i]; b {
 			checked = "x"
+		} else {
+			allCompleted = false
 		}
 
 		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
 	}
-	if m.textInputFocus {
-		s += fmt.Sprintf("%s\n", m.textInput.View())
+	if m.textInputFocus && !m.editing {
+		s += fmt.Sprintf("  [ ] %s\n", m.textInput.View())
+	}
+	if allCompleted && (!m.textInputFocus || m.editing) {
+		s += fmt.Sprintf("\nAll tasks completed ✅")
+	} else {
+		s += fmt.Sprintln()
 	}
 	return s
 }
@@ -108,7 +148,7 @@ func (m model) View() string {
 func main() {
 	p := tea.NewProgram(initialModel())
 	if err := p.Start(); err != nil {
-		fmt.Printf("Alas, there's been an error: %v", err)
+		fmt.Printf("error: %v", err)
 		os.Exit(1)
 	}
 }
